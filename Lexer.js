d82629b7e4d72a177fe8988fb4ef31d382a83df7
lightboomq <pikalov.ea97@gmail.com>
2024-12-29 17:35:12 +0300

    t3

--- Lexer.js
+++ Lexer.js
@@ -1,13 +1,10 @@
-const tableTokens = document.querySelector('.tableTokens');
-const input = document.querySelector('.inputCode');
-const btn = document.querySelector('.btn')
+const tableTokens = document.querySelector('.tableTokens'); 
+const input = document.querySelector('.inputCode'); 
+const btn = document.querySelector('.btn') 
 
 
-
-
-
-class Token {
-    constructor( token, name, row, pos, length) {
+class Token { 
+    constructor( token, name, row, pos, length) { 
         this.token = token;
         this.name = name
         this.row = row;
@@ -16,107 +13,86 @@ class Token {
     }
 }
 
-
-const typeList = [
-    {token: 'keyword' , regex: `^(break|case|catch|class|const|continue|debugger|default|delete|do|else|export|extends|finally|for|function|if|import|in|instanceof|let|new|return|super|switch|this|throw|try|typeof|var|void|while|width|yield)`},
-                                  
-
-    {token:'literal', regex: '^(\\d{1,}|null|undefined|true|false)'},
+console.log()
+const typeList = [ 
+    {token: 'new line' , regex: '^[\\n]'},
     {token: 'space' , regex: '^[\\s]'},
-    {token: '\n' , regex: '^[\\n]'},
-    {token:'aritmetic-operator', regex: '^(\\+|-|\\*|\\/|%)'},
-    {token:'assignment-operator', regex: '^(=|\\+=|-=|\\*=|\\/=)'},
-    {token:'logical-operator', regex: '^(&&|!|\\|\\|)'},
-    {token:'compare-operator', regex: '^(==|===|!=|<|>|<=|>=)'},
+    {token: 'keyword' , regex: `^(map|filter|break|case|catch|class|constructor|const|continue|debugger|default|delete|do|else|export|extends|finally|for
+                                 |function|if|import|in|instanceof|let|new|return|super|switch|this|throw|try|typeof|var|void|for|while|width|yield)`},
+    {token:'literal', regex: '^(\\d{1,}|null|undefined|true|false)'},
+    {token:'aritmetic operator', regex: '^(\\+|-|\\*|\\/|%)'},
+    {token:'assignment operator', regex: '^(=|\\+=|-=|\\*=|\\/=)'},
+    {token:'logical operator', regex: '^(&&|!|\\|\\|)'},
+    {token:'compare operator', regex: '^(==|===|!=|<|>|<=|>=)'},
     {token:'punctuation', regex: '^(,|;|\\(|\\)|\\{|\\}|\\[|\\]|:|\\.)'},
-    {token:'template-literals', regex: '^${'},
+    {token:'template literals', regex: '^${'},
     {token:'ident', regex: '^\\w{1,}'},
 ]
 
 class Lexer {
-    tokenList = [];
+    tokenList = []; 
     
     constructor(code, row = 1, pos = 0, length = 0) {
-        this.code = `const let case class`;
+        this.code = code;
         this.row = row
         this.pos = pos
         this.length = length
     }
 
-    render(tokenList){
-        for(let i=0; i<tokenList.length; i++){
-            const token = document.createElement('div');
-            token.style.textAlign = 'center'
-            token.textContent = `${tokenList[i].token}: ${tokenList[i].name}`;
-            tableTokens.appendChild(token)
-
-            const row = document.createElement('div');
-            row.style.textAlign = 'center'
-            row.textContent = tokenList[i].row
-            tableTokens.appendChild(row)
-
-            const pos = document.createElement('div');
-            pos.style.textAlign = 'center'
-            pos.textContent = tokenList[i].pos
-            tableTokens.appendChild(pos)
-
-            const length = document.createElement('div');
-            length.style.textAlign = 'center'
-            length.textContent = tokenList[i].length
-            tableTokens.appendChild(length)
+    render(tokenList){     
+        for(let i=0; i<tokenList.length; i++){ 
+            this.createEl(`${tokenList[i].token}: ${tokenList[i].name}`, i)
+            this.createEl(tokenList[i].row, i)
+            this.createEl(tokenList[i].pos, i)
+            this.createEl(tokenList[i].length, i)
         }
-        return this.tokenList
     }
-
+    createEl(token, i){       
+        const el = document.createElement('div');
+        if(i % 1=== 0 ) el.style.borderRight = '0px solid black';
+        el.classList.add('row');
+        el.textContent = token;
+        tableTokens.appendChild(el);
+    }
     
-    nextToken(){   
-        if (this.pos >= this.code.length){
-            this.tokenList = this.tokenList.filter(item => item.token !== 'space'  && item.token !== '\n' && item.text !== ' ')
-            return this.render(this.tokenList);
+    nextToken(){  
+        if (this.pos >= this.code.length){ 
+            this.tokenList = this.tokenList.filter(item => item.token !== 'space' && item.token !== 'new line' && item.text !== ' ') 
+            return this.render(this.tokenList); 
         } 
 
-        
-        for (let i = 0; i < typeList.length; i++) {      
-            const currentRegex = new RegExp(typeList[i].regex);
-            const currentToken = typeList[i].token
-            const currentStr = this.code.slice(this.pos)
-            
+        for (let i = 0; i < typeList.length; i++) {   
+            const currentRegex = new RegExp(typeList[i].regex); 
+            const currentToken = typeList[i].token 
+            const currentStr = this.code.slice(this.pos)  
             const result = currentStr.match(currentRegex); 
-           
-            console.log(currentRegex)
-            console.log(currentStr)
-            if(result && result[0]) {
+            
+            if(result && result[0]) { 
+                this.pos += result[0].length; 
+                const name = result[0]; 
+                const rowToken = currentToken === 'new-line' ? this.row++ : this.row  
+                const lengthToken = result[0].length 
+                const token = new Token(currentToken, name, rowToken, this.pos, lengthToken); 
                 
-                this.pos += result[0].length;
-                const name = result[0];
-                const rowToken = currentToken === '\n' ? this.row++ : this.row    
+                this.tokenList.push(token); 
                 
-                const lengthToken = result[0].length
-                const token = new Token(currentToken, name, rowToken, this.pos, lengthToken);
-                
-                this.tokenList.push(token);
-                
-                return this.nextToken();
+                return this.nextToken(); 
             }
         }
-        throw new Error(`На позиции ${this.pos} обнаружена ошибка`)
+        throw new Error(`На позиции ${this.pos} обнаружена ошибка`) 
     }
 }
 
-const lexer = new Lexer();
-lexer.nextToken()
 
 
-btn.addEventListener('click',()=>{
-    const code = input.value;
-    console.log(code)
-    const lexer = new Lexer(`for(lesdgt i = 0 
-)`);
-    lexer.lexAnalysis()
+btn.addEventListener('click',()=>{ 
+    const code = input.value; 
+    const lexer = new Lexer(code) 
+    lexer.nextToken() 
 })
 
-
-
+const lexer = new Lexer('for{let i = 0; i<20; i++') 
+lexer.nextToken() 
 
 
 
@@ -142,16 +118,6 @@ btn.addEventListener('click',()=>{
 //     'RBRACE': new TokenType('}', '^\\}'),
 //     'IDENT': new TokenType('ident', '^[a-z]*'),
 // }
-
-
-
-
-
-
-
-
-
-
 
 
 

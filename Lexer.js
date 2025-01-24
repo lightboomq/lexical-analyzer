38f9eac372a18ab022cf693406dbbc2a449c664d
lightboomq <pikalov.ea97@gmail.com>
2025-01-24 21:59:30 +0300

    release

--- Lexer.js
+++ Lexer.js
@@ -14,18 +14,20 @@ class Token {
 }
 
 const typeList = [ 
+    {token:'compare operator', regex: '^(==|===|!=|<|>|<=|>=)'},
     {token: 'new line' , regex: '^[\\n]'},
     {token: 'space' , regex: '^[\\s]'},
     {token: 'keyword' , regex: `^(map|filter|break|case|catch|class|constructor|const|continue|debugger|default|delete|do|else|export|extends|finally|for
                                  |function|if|import|in|instanceof|let|new|return|super|switch|this|throw|try|typeof|var|void|for|while|width|yield|console|log)`},
-    {token:'literal', regex: '^(\\d{1,}|null|undefined|true|false)'},
+    {token:'literal', regex: '^(\\d{1,}|null|undefined|true|false|`)'},
     {token:'aritmetic operator', regex: '^(\\+|-|\\*|\\/|%)'},
     {token:'assignment operator', regex: '^(=|\\+=|-=|\\*=|\\/=)'},
     {token:'logical operator', regex: '^(&&|!|\\|\\|)'},
-    {token:'compare operator', regex: '^(==|===|!=|<|>|<=|>=)'}, //не срабатывает регулярка на <|>|<=|>=
+    {token:'compare operator', regex: '^(==|===|!=|<|>|<=|>=)'},
     {token:'punctuation', regex: '^(,|;|\\(|\\)|\\{|\\}|\\[|\\]|:|\\.)'},
     {token:'template literals', regex: '^${'},
     {token:'ident', regex: '^\\w{1,}'},
+
 ]
 
 class Lexer {
@@ -38,18 +40,9 @@ class Lexer {
         this.length = length
     }
 
-    syntaxErrors(){
-        // try{
-        //     const res = eval(this.code);
-        //     console.log(res)
-        // }catch(err){
-        //     errors.textContent = err
-        // }
-    }
-
     render(tokenList){     
         for(let i=0; i<tokenList.length; i++){ 
-            this.createEl(`${tokenList[i].token}: `, `${tokenList[i].name}`,tokenList[i].pos)
+            this.createEl(`${tokenList[i].token}: `, `${tokenList[i].name}`, tokenList[i].pos)
             this.createEl(tokenList[i].row)
             this.createEl(tokenList[i].pos)
             this.createEl(tokenList[i].length)
@@ -80,7 +73,7 @@ class Lexer {
         tableTokens.appendChild(li);
     }
     
-    nextToken(){  
+    nextToken(){ 
         if (this.pos >= this.code.length){ 
             this.tokenList = this.tokenList.filter(item => item.token !== 'space' && item.token !== 'new line' && item.text !== ' ') 
             return this.render(this.tokenList); 
@@ -88,8 +81,9 @@ class Lexer {
 
         for (let i = 0; i < typeList.length; i++) {   
             const currentRegex = new RegExp(typeList[i].regex);
-            const currentToken = typeList[i].token 
-            const currentStr = this.code.slice(this.pos)  
+            const currentToken = typeList[i].token;
+            let currentStr = this.code.slice(this.pos); 
+        
             const result = currentStr.match(currentRegex); 
             
             if(result !== null) { 
@@ -105,19 +99,19 @@ class Lexer {
             }
         }
         errors.textContent = `На позиции ${this.pos} обнаружен не существующий токен языка`
-        throw new Error(`На позиции ${this.pos} обнаружен не существующий токен языка`)
     }
 }
 
 btn.addEventListener('click',()=>{
     tableTokens.textContent = '';
+    errors.textContent = 'Ошибки...';
+
     let code = input.innerHTML; 
-    code = code.replace(/<div>/g, '\n').replace(/<\/div>/g, '');
-    const lexer = new Lexer(code)
-    lexer.syntaxErrors() 
+    code = getReplacedHtmlMarkup(code)
+   
+    const lexer = new Lexer(code) 
     lexer.nextToken() 
 })
-
 
 tableTokens.addEventListener('click', (e)=>{
     if(e.target.tagName !== 'SPAN') return
@@ -126,19 +120,29 @@ tableTokens.addEventListener('click', (e)=>{
     const selectedToken = spanTokenName.textContent;
     const posToken = Number(spanTokenName.getAttribute('pos'));
 
-    let code = input.innerHTML
-    code = code.replace(/<div>/g, '\n').replace(/<\/div>/g, '');
-    code = code.replace(/<span[^>]*>/g, '').replace(/<\/span>/g, '');
-     
-    highlightToken(code,selectedToken,posToken);
+    let code = input.innerHTML; 
+    code = getReplacedHtmlMarkup(code);
+   
+    getHighlightToken(code,selectedToken,posToken);
 })
 
 
-function highlightToken(code,selectedToken,posToken) {
+function getReplacedHtmlMarkup(code){
+    let updateCode;
+    updateCode = code.replace(/<div>/g, '\n').replace(/<\/div>/g, '');
+    updateCode = code.replace(/<span[^>]*>/g, '').replace(/<\/span>/g, '');
+    updateCode = code.replace(/&gt;/g,'>')
+    updateCode = code.replace(/&lt;/g,'<')
+
+    return updateCode;
+} 
+
+function getHighlightToken(code,selectedToken,posToken) {
     const span = `<span class='highlight'>${selectedToken}</span>`;
-    const updateInput = code.slice(0, posToken-selectedToken.length) + span + code.slice(posToken,);  
-                     
-    input.innerHTML = updateInput;
+    let updateInput = code.slice(0, posToken-selectedToken.length) + span + code.slice(posToken,);  
+    updateInput = getReplacedHtmlMarkup(updateInput)
+    
+    input.innerHTML = updateInput; 
 }
 
 
